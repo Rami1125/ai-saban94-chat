2eae49e8b4688b2b24c31aca21e14cce55ac5aee
Rami1125 <rami.msarwa1@gmail.com>
2026-03-19 15:46:52 +0200

    Create page.tsx

---
+++ app/ai-brain/page.tsx
@@ -0,0 +1,12 @@
+import SabanAIChat from "@/components/SabanAIChat";
+
+export default function AIControlPage() {
+  return (
+    <div className="relative min-h-screen">
+      {/* הקוד של דף הסידור שלך כאן... */}
+      
+      {/* הצ'אט הצף */}
+      <SabanAIChat />
+    </div>
+  );
+}
